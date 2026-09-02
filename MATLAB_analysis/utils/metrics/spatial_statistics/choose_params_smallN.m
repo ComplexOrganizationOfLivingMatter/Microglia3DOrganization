a8d16f062dd71551e8716fbed0ba68b8cde4cbf8
ElenaBell <103112623+ElenaBell@users.noreply.github.com>
2026-09-02 10:21:29 +0200

    Simplify parameter selection for smallN
    Removed conditional logic for maximum radius and number of bins.

--- MATLAB_analysis/utils/metrics/spatial_statistics/choose_params_smallN.m
+++ MATLAB_analysis/utils/metrics/spatial_statistics/choose_params_smallN.m
@@ -8,13 +8,9 @@ function params = choose_params_smallN(numberOfPoints, sphereRadius)
 % Output
 %   params         Structure containing rmax and equal-volume bin edges.
 
-    if numberOfPoints <= 30
-        maximumRadius = 0.7 * sphereRadius;
-        numberOfBins = 12;
-    else
-        maximumRadius = 0.8 * sphereRadius;
-        numberOfBins = 16;
-    end
+    maximumRadius = 0.7 * sphereRadius;
+    numberOfBins = 12;
+
     binIndex = 0:numberOfBins;
     edges = maximumRadius * (binIndex / numberOfBins).^(1/3);
     params = struct('rmax', maximumRadius, 'edges_g', edges);
